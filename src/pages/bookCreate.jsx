7b07f1d5b334077b7e770be3c28c3bb271412161
shadyn <shadyndyn234@gmail.com>
2024-10-28 18:35:55 +0100

    arreglos realizados

--- src/pages/bookCreate.jsx
+++ src/pages/bookCreate.jsx
@@ -56,47 +56,105 @@ const BookCreate = () => {
         navigate('/book-journal');
     };
 
-    return (
+    return  (
         <div className="container">
             <div>
-                <h2 className="text-center mb-4">Crear Nuevo Libro</h2>
-                <button onClick={handleReadClick} className="btn-back">Volver</button>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label className="form-label">Título:</label>
-                        <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Autor:</label>
-                        <input type="text" className="form-control" value={author} onChange={(e) => setAuthor(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Fecha de inicio:</label>
-                        <input type="date" className="form-control" value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Fecha de finalización:</label>
-                        <input type="date" className="form-control" value={finishDate} onChange={(e) => setFinishDate(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Número de páginas:</label>
-                        <input type="number" className="form-control" value={pages} onChange={(e) => setPages(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Calificación (0 a 5):</label>
-                        <input type="number" className="form-control" step="0.1" value={rating} onChange={(e) => setRating(e.target.value)} required />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Imagen del libro:</label>
-                        <input type="file" className="form-control" onChange={handleFileChange} />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Color de resaltado:</label>
-                        <input type="color" className="form-control" value={highlightColor} onChange={(e) => setHighlightColor(e.target.value)} />
-                    </div>
-                    <button type="submit" className="btn btn-success w-100">Crear</button>
-                </form>
+            <h2>Crear Nuevo Libro</h2>
+            <button type="button" className="btn-back" onClick={handleReadClick}>Volver</button>
             </div>
+
+            <form onSubmit={handleSubmit}>
+                <div className="form-grid">
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="title">Título:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="author">Autor:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="author"
+                            value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="startDate">Fecha de inicio:</label>
+                        <input
+                            type="date"
+                            className="form-control"
+                            id="startDate"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="finishDate">Fecha de finalización:</label>
+                        <input
+                            type="date"
+                            className="form-control"
+                            id="finishDate"
+                            value={finishDate}
+                            onChange={(e) => setFinishDate(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="pages">Número de páginas:</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="pages"
+                            value={pages}
+                            onChange={(e) => setPages(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="rating">Calificación (0 a 5):</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="rating"
+                            step="0.1"
+                            value={rating}
+                            onChange={(e) => setRating(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="imageFile">Imagen del libro:</label>
+                        <input
+                            type="file"
+                            className="form-control"
+                            id="imageFile"
+                            accept="image/*"
+                            onChange={handleFileChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="highlightColor">Color de resaltado:</label>
+                        <input
+                            type="color"
+                            className="form-control"
+                            id="highlightColor"
+                            value={highlightColor}
+                            onChange={(e) => setHighlightColor(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button type="submit" className="btn-success">Crear</button>
+            </form>
         </div>
     );
 };
