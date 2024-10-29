88f3052a9f77d0e3040657176786d448ebb4461e
shadyn <shadyndyn234@gmail.com>
2024-10-29 19:44:32 +0100

    bototnes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,24 @@ import {  useNavigate } from 'react-router-dom';
 
 const Home = ()=>{
     const navigate = useNavigate();
-
-
+    
     const handleBook = ()=>{
         navigate('/book-journal');
-
     }
-
     const handleMovies = ()=>{
         navigate('/movieInfo');
     }
 
 
-    
+
     return(
         <div>
+
+            <div className='buttons-div'>
             <button onClick={handleMovies}>Movies Info</button>
-            <button  onClick={handleBook}>Book Journal</button>
-            <p></p>
+            <button  onClick={handleBook}>Book Journal</button>  
+            </div>
+           
 
         </div>
     )
